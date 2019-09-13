a60be4d40fcab2a63aaeb4577d0d92c6144fc520
okno <paul.delafosse@protonmail.com>
2019-09-13 17:15:12 +0200

    wip scrot

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 date=$(date +%Y-%m-%dT%H:%M:%S%z)
-grim -g "$(slurp)" "$HOME/Pictures/screenshot$date.png" && paplay "$HOME/dotfiles/scripts/screenshot.wav"
+scrot -s  "$HOME/Pictures/screenshot$date.png" && paplay "$HOME/dotfiles/scripts/screenshot.wav"
