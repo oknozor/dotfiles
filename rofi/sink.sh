62320681cc0128e65a29d1e51751abfccb33c5bb
okno <paul.delafosse@protonmail.com>
2019-10-01 14:52:05 +0200

    feat(): passmenu + intellij assign

--- rofi/sink.sh
+++ rofi/sink.sh
@@ -2,20 +2,20 @@
 
 if [ -z $@ ]
 then
-function get_sinks ()
-{
-    pac_list=$(pactl list)
-    sink=$(echo $pac_list |grep 'Sink #' | awk -F'#' '{ print $2 }')
-    args=$(echo $pac_list |grep "Sink #$sink" -A 4)
-    sink_name=$(echo $args | grep "Name" | awk -F': ' '{ print $2 }') 
-    state=$(echo $args | grep "State" | awk -F': ' '{ print $2 }') 
+    function get_sinks()
+    {
+        pac_list=$(pactl list)
+        sink=$(echo $pac_list |grep 'Sink #' | awk -F'#' '{ print $2 }')
+        args=$(echo $pac_list |grep "Sink #$sink" -A 4)
+        sink_name=$(echo $args | grep "Name" | awk -F': ' '{ print $2 }') 
+        state=$(echo $args | grep "State" | awk -F': ' '{ print $2 }') 
 
-    echo "$sink : $sink_name - State : $state " 
-    return "$sink"
-}
+        echo "$sink : $sink_name - State : $state " 
+        return "$sink"
+    }
 
 
-echo "empty"; get_sinks
+get_sinks
 else
     SINK=$@
 
@@ -24,7 +24,6 @@ else
         sink.sh >/dev/null
     elif [ -n "${SINK}" ]
     then
-        echo $($SINK 
-        set CURRENT_SINK="${SINK}" | head -c 1 >/dev/null
+        echo "$SINK"
     fi
 fi
