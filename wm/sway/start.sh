10ada61dffb1976e5a3bee33e12a1d2972428643
Paul Delafosse <paul.delafosse@protonmail.com>
2022-06-14 15:32:16 +0200

    feat: eww rewrite

--- wm/sway/start.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-# first import environment variables from the login manager
-systemctl --user import-environment
-# then start the service
-exec systemctl --wait --user start sway.service
