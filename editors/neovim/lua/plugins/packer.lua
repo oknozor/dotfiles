fb851af8f38c2bfb596bf14d1b69982aa0025d0e
Paul Delafosse <paul.delafosse@protonmail.com>
2023-02-01 08:54:41 +0100

    feat: back to waybar

--- editors/neovim/lua/plugins/packer.lua
+++ editors/neovim/lua/plugins/packer.lua
@@ -1,11 +1,15 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-local is_boostrap = false
-if fn.empty(fn.glob(install_path)) > 0 then
-  install_path = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 require('packer').startup(function(use)
   -- Style
@@ -105,10 +109,15 @@ require('packer').startup(function(use)
     end
   }
 
-
-
-  if is_boostrap then
+  if packer_bootstrap then
     require('packer').sync()
   end
 
 end)
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
--- wm/bar.sh
+++ wm/bar.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
 CONFIG=$HOME/.config/eww
 eww -c ${CONFIG} kill || true && eww -c ${CONFIG} daemon && eww -c ${CONFIG} open bar
