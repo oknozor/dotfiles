9a786ea58e1139b427248350a6ff0cd0c164a47d
Paul Delafosse <paul.delafosse@protonmail.com>
2024-06-07 14:31:30 +0200

    feat: add a bar and cleanup some stuff

--- editors/neovim/init.lua
+++ editors/neovim/init.lua
@@ -3,14 +3,14 @@ vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -18,23 +18,23 @@ vim.opt.rtp:prepend(lazypath)
 require("opts")
 require("keymap")
 require("lazy").setup({
-	{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
-	"nvim-lualine/lualine.nvim",
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{ "numtostr/comment.nvim", opts = {} },
-	{ require("plugins.telescope") },
-	{ "lvimuser/lsp-inlayhints.nvim" },
-	{ require("plugins.lsp-config") },
-	{ require("plugins.crates") },
-	{ require("plugins.nvim-cmp") },
-	{ require("plugins.treesitter") },
-	{ require("plugins.conform") },
-	{ require("plugins.neotree") },
-	{ require("plugins.saga") },
-	{ require("plugins.autopair") },
+    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+    "nvim-lualine/lualine.nvim",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    { "catppuccin/nvim",             name = "catppuccin",           priority = 1000 },
+    { "numtostr/comment.nvim",       opts = {} },
+    { require("plugins.telescope") },
+    { "lvimuser/lsp-inlayhints.nvim" },
+    { require("plugins.lsp-config") },
+    { require("plugins.crates") },
+    { require("plugins.nvim-cmp") },
+    { require("plugins.treesitter") },
+    { require("plugins.conform") },
+    { require("plugins.neotree") },
+    { require("plugins.saga") },
+    { require("plugins.autopair") }
 })
 
 require("au")
